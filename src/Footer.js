1f9d3d45b46a1bea5840d0f5e55d7af278e30832
Abhishek Gupta <ravi37463@gmail.com>
2022-09-21 02:02:40 +0530

    initial-commit

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <Footer>
+            <span>Copywrite@Coder-cops</span>
+            <a href="#">Feedback</a>
+        </Footer>
+    )
+}
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function Header() {
+    return (
+        <header>
+            <ul>
+                <li>Alumni</li>
+                <li>Faculty</li>
+                <li>Administration</li>
+            </ul>
+            <button>Admission</button>
+        </header>
+    )
+}
+
+
+
