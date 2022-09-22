172c387ed084354483bca05ca3797599b12b3060
hypertext-assains-ietagra <airiraghav@gmail.com>
2022-09-22 22:39:07 +0530

    Component/NavbarStarting

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,7 @@ export default function Header() {
     return (
         <header>
             <ul>
-                <li>Alumni</li>
-                <li>Faculty</li>
-                <li>Administration</li>
+
             </ul>
             <button>Admission</button>
         </header>
