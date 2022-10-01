2adb7dbd3975184d78da5b3d8b3fe3fcbfe4b838
hypertext-assains-ietagra <airiraghav@gmail.com>
2022-10-01 11:23:37 +0530

    fix Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-
 import './App.css';
-import Navbar from "./Navbar";
+import 'bootstrap/dist/css/bootstrap.css'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import logo from './image.png';
 
 function App() {
   return (
-    <div className="container">
-                <Navbar />
-          <div className="container">
-            <article>
-              <h1>
-                Hola :)
-              </h1>
-            </article>
+    <div className="App">
+      <Navbar bg="dark" variant="dark"
+        sticky="top" expand="sm" collapseOnSelect>
+
+<Navbar.Brand>
+          <img src={logo} width="40px" height="40px" />{' '}
+          Home
+        </Navbar.Brand>
+
+        <Navbar.Toggle className="coloring" />
+        <Navbar.Collapse>
+          <Nav>
+          <Nav.Link href="#about-us">About Us</Nav.Link>
+            <NavDropdown title="Academics">
+              <NavDropdown.Item href="#academics/results">Results</NavDropdown.Item>
+              <NavDropdown.Item href="#academics/scheduler">Schedule</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#blog">Blog</Nav.Link>
+
+            <Nav.Link href="#contact-us">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+
+      </Navbar>
       <header className="App-header">
-        <h1>Fuck off!</h1>
+        <h1>IET</h1>
       </header>
-          </div>
     </div>
   );
 }
 
 export default App;
+
